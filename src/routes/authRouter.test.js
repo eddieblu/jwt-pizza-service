c00aec916f41b58b08c74b3974d4957b659ce81a
Bethany Edwards <bethany.edwards@cocooncs.com>
2025-01-28 15:12:00 -0700

    class work . tests in authrouter

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,3 +1,49 @@
-test('hello world', () => {
-    expect('hello' + ' world').toBe('hello world');
-  });
+const request = require('supertest');
+const app = require('../service');
+
+const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+let testUserAuthToken;
+
+// NOTES: before any of the tests runs this code will run. Not before each one, only once per run of tests. 
+beforeAll(async () => {
+  testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+  const registerRes = await request(app).post('/api/auth').send(testUser);
+  testUserAuthToken = registerRes.body.token;
+  expectValidJwt(testUserAuthToken);
+});
+
+test('register', async () => {
+  const user = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+  user.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+  const registerRes = await request(app).post('/api/auth').send(user);
+  expect(registerRes.status).toBe(200);
+
+  // NOTES: to make sure tests are doing what they should, you can debug the test and go through each step 
+
+  // NEED MORE FOR A GOOD TEST -- IMPLEMENT LATER
+  // expectValidJwt(registerRes.body.token);
+  // const expectedUser = { ...testUser, roles: [{ role: 'diner' }] };
+  // delete expectedUser.password;
+  // expect(registerRes.body.user).toMatchObject(expectedUser);
+});
+
+test('register without password', async () => {
+  const user = { name: 'pizza diner', email: 'reg@test.com', password: '' };
+  user.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+  const registerRes = await request(app).post('/api/auth').send(user);
+  expect(registerRes.status).toBe(400);
+});
+
+test('login', async () => {
+  const loginRes = await request(app).put('/api/auth').send(testUser);
+  expect(loginRes.status).toBe(200);
+  expectValidJwt(loginRes.body.token);
+
+  const expectedUser = { ...testUser, roles: [{ role: 'diner' }] };
+  delete expectedUser.password;
+  expect(loginRes.body.user).toMatchObject(expectedUser);
+});
+
+function expectValidJwt(potentialJwt) {
+  expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+}
