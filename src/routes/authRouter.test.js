cc0a318bc0b876852216d56ef39d997de9336f60
Bethany Edwards <bethany.edwards@cocooncs.com>
2025-01-30 18:31:53 -0700

    add lint, add test for changing user info

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -58,8 +58,32 @@ test('logout', async () => {
   expect(logoutRes.body.message).toBe('logout successful');
 });
 
+test('change user email', async () => {
+  const user = { name: 'pizza diner', email: '', password: 'a' };
+  user.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+  const registerRes = await request(app).post('/api/auth').send(user);
 
+  expect(registerRes.status).toBe(200);
+  expect(registerRes.body.user).toHaveProperty('id');
+  const userId = registerRes.body.user.id;
 
+  const loginRes = await request(app).put('/api/auth').send(user);
+  expect(loginRes.status).toBe(200);
+  const token = loginRes.body.token;
+  expectValidJwt(token);
+
+  const newEmail = `updated_${Math.random().toString(36).substr(2, 8)}@example.com`;
+  await request(app).put(`/api/auth/${userId}`).set('Authorization', `Bearer ${token}`).send({ email: newEmail });
+
+  const logoutRes = await request(app).delete('/api/auth').set('Authorization', `Bearer ${token}`);
+  expect(logoutRes.status).toBe(200);
+  expect(logoutRes.body.message).toBe('logout successful');
+
+  const userWithNewEmail = { name: 'pizza diner', email: newEmail, password: 'a' };
+  const newLoginRes = await request(app).put('/api/auth').send(userWithNewEmail);
+  expect(newLoginRes.status).toBe(200);
+  expectValidJwt(newLoginRes.body.token);
+});
 
 function expectValidJwt(potentialJwt) {
   expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
